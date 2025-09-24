d778b3fdf7dd5c683a6a85a8e1c239fbef13fe48
aeapodaca <aeapodaca@miners.utep.edu>
2025-09-24 09:18:49 -0600

    Update casa_6.py Abraham Apodaca

--- casa_6.py
+++ casa_6.py
@@ -1,1 +1,8 @@
 filename = './big-mac-full-index.csv'
+count = 0
+with open(filename, "r", encoding="utf-8") as f:
+  for line in f:
+    if "USA" in line: 
+        print(line.strip())
+        count += 1
+print("total lines found with 'USA' :", count)
